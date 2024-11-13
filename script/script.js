a1e236033cf894205ec105e5568d3cffdb425b9e
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2024-11-13 15:35:36 +0100

    6 card create

--- script/script.js
+++ script/script.js
@@ -36,3 +36,27 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+ let template = "";
+
+ const team = document.getElementById("team");
+
+ for(let value of teamMembers){
+    template += 
+    `<div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="/assets/${value.img}" class="img-fluid rounded-start" alt="${value.name}">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${value.name}</h5>
+            <p class="card-text">${value.role}</p>
+            <p class="email">${value.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>`
+ }
+
+ team.innerHTML = template;
